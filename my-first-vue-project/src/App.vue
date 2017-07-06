4b8cc3466dd0ba80b3196a23820c54b02c2a8ed2
Dida1209 <duhuiying1209@Outlook.com>
2017-07-06 14:54:39 +0800

    my-first-vue-project

---
+++ my-first-vue-project/src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="app">
+    <h1>{{title}}</h1>
+    <input v-model='newItem' @keyup.enter='addNew'>
+    <ul>
+      <li v-for='item in items' 
+      v-bind:class='{finished:item.isFinished}'
+      v-on:click='toggleFinish(item)'
+      >
+        {{item.label}}
+      </li>
+    </ul>
+    <p>child tells me :{{childword}}</p>
+    <component-a msgFather='father and child' v-on:child-function="childFunction"></component-a>
+  </div>
+ </template>
+
+<script>
+// import Hello from './components/Hello'
+import Store from './store'
+import ComponentA from './components/componentA'
+
+export default {
+  data: function () {
+    return {
+      title: 'this is a todo list!',
+      items: Store.fetch() ,
+      newItem: ''   ,
+      childword:''  
+    }
+  },
+  components: {
+    ComponentA
+  },
+  watch: {
+    items: {
+      handler:function(items){
+        Store.save(items);
+      },
+      deep: true
+    }
+  },
+  events:{
+    'child-function':function(msg){
+      this.childword=msg;
+    }
+  },
+  methods: {
+    toggleFinish: function(item){
+      item.isFinished = !item.isFinished;
+    },
+    addNew: function(){
+      this.items.push({
+        label:this.newItem,
+        isFinished:false
+      })
+      this.newItem='';
+    }
+  }
+}
+
+</script>
+
+<style>
+.finished {
+  text-decoration: underline;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-first-vue-project/src/components/componentA.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button v-on:click='onClickMe'>emit function!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'hello componentA'
+    }
+  },
+  props: ["msgFather"],
+  methods:{
+  	onClickMe:function(){
+  		console.log(this.msgFather);
+  		this.$dispatch('child-function','emit function on');
+  	}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
