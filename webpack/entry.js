d3ab630e237585d1de4f8395469111f8e5ef351d
Dida1209 <duhuiying1209@Outlook.com>
2017-07-17 09:32:16 +0800

    my first webpack

---
+++ webpack/entry.js
@@ -0,0 +1,3 @@
+document.getElementById('app').innerHTML="这是我第一个用webpack打包的程序";
+//添加
+require("./first.js");
---
+++ webpack/first.js
@@ -0,0 +1,3 @@
+var h2 = document.createElement("h2");
+h2.innerHTML = "这是第二个要打包的webpack程序";
+document.body.appendChild(h2);
