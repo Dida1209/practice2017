13e2df44aa2c0d63e00f21223dc95c848bbc3487
Dida1209 <duhuiying1209@Outlook.com>
2017-08-14 15:11:52 +0800

    echarts in vue

--- vue-collapse/src/App.vue
+++ vue-collapse/src/App.vue
@@ -1,51 +1,28 @@
 <template>
     <div id="app">
-      <div class="collapse-title">
-          <a @click="showCollapse" >点击打开collapse</a>
-      </div>
-      <transition name="fade">
-        <div class="collapse-layer in" v-show="isShowCollapse">
-            <div id="targetCollapse" class="collapse-content">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-            </div>
-          </div>
-      </transition>
+        <st-collapse :collapseArray="collapseData"></st-collapse>
     </div>
 </template>
 
 <script>
+  import Collapse from './components/Hello.vue';
 export default {
   name: 'app',
+  components:{'st-collapse':Collapse},
   data(){
     return {
-      isShowCollapse: false
-    }
-  },
-  computed:{
-    targetHeight: function (){
-      console.log(this.$el('#targetCollapse'));
-    }
-  },
-  mounted(){
-    var element = this.$el.getElementsByClassName('collapse-content')[0];
-      console.log(window.getComputedStyle(element,null).height);
-      console.log((element.currentStyle? element.currentStyle : window.getComputedStyle(element, null)).height);
-      console.log(element.offsetHeight);
-  },
-  methods: {
-    showCollapse: function () {
-      this.isShowCollapse = !this.isShowCollapse;
-      var element = this.$el.getElementsByClassName('collapse-content')[0];
-      console.log(window.getComputedStyle(element,null).height);
-      console.log((element.currentStyle? element.currentStyle : window.getComputedStyle(element, null)).height);
-      console.log(element.offsetHeight);
-    }
-  },
-  create:{
+        collapseData:[
+            {
+                title: '点击打开collapse',
+                isShowCollapse: false,
+                content: "labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS."
+            },{
+                title: '点击打开collapse',
+                isShowCollapse: false,
+                content: "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS."
+            }
+        ]
 
-  },
-  watch:{
-    isShowCollapse: function(){
 
     }
   }
@@ -53,45 +30,5 @@ export default {
 </script>
 
 <style>
-        .collapse-title{
-            padding:10px 15px;
-            border:1px solid #ddd;
-            background: #f5f5f5;
-            border-radius: 3px;
-            font-size: 16px;
-        }
-        .collapse-title a{
-            cursor: pointer;
-        }
-        .collapse-layer{
-            border: 1px solid #ddd;
-            color:#333;
-            font-size:14px;
-            background: #fff;
-            margin-top:-3px;
-        }
-        .collapse-content {
-            padding: 15px;
-            min-height: 20px;
-        }
-        .fade-enter-active, .fade-leave-active {
-          transition: opacity .5s
-        }
-        .fade-enter, .fade-leave-to /* .fade-leave-active in below version 2.1.8 */ {
-          opacity: 0
-        }
-        .in {
-          display: block;
-        }
-        /*.in{
-            animation: collapseIn 5s;
-        }*/
-        /*@keyframes collapseIn {
-            0%{
-                height: 0;
-            }
-            100%{
-                height: auto;
-            }
-        }*/
+
 </style>
--- vue-collapse/src/components/Hello.vue
+++ vue-collapse/src/components/Hello.vue
@@ -1,31 +1,52 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div id="hello">
+    <div  v-for="(collapseItem, index) in collapseArray">
+      <div class="collapse_title">
+        <a @click="showCollapse(index)" >{{collapseItem.title}}</a>
+      </div>
+      <div :class="{collapse_layer:true,collapseIn: collapseItem.isShowCollapse,collapseOut:!collapseItem.isShowCollapse}">
+        <div class="collapse_content">
+          {{collapseItem.content}}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'hello',
-  data () {
+  data(){
     return {
-      msg: 'Welcome to Your Vue.js App'
+//      isShowCollapse: false
+    }
+  },
+  props:['collapseArray'],
+  mounted(){
+      var elementContent = this.$el.getElementsByClassName('collapse_content');
+      console.log('start mounted',elementContent);
+      for(var i=0; i<elementContent.length; i++){
+//          console.log(elementContent[i].offsetHeight);
+          this.collapseArray[i].height = elementContent[i].offsetHeight ;
+//          this.collapseArray[i].height = (elementContent[i].currentStyle? elementContent[i].currentStyle : window.getComputedStyle(elementContent[i], null)).offsetHeight;
+      }
+      console.log(elementContent,this.collapseArray);
+
+  },
+  methods: {
+    showCollapse: function (index) {
+        this.collapseArray[index].isShowCollapse = !this.collapseArray[index].isShowCollapse;
+        var eleParent = this.$el.getElementsByClassName('collapse_layer')[index];
+        var oHeight = this.collapseArray[index].height;
+        if(this.collapseArray[index].isShowCollapse){
+          eleParent.style.height = oHeight + 'px';
+          eleParent.style.opacity = '1';
+          console.log(eleParent.style.height);
+        }else{
+          eleParent.style.height = '0';
+          eleParent.style.opacity = '0';
+
+        }
+
     }
   }
 }
@@ -33,21 +54,61 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  .collapse_title{
+    padding:10px 15px;
+    border:1px solid #ddd;
+    background: #f5f5f5;
+    border-radius: 3px;
+    font-size: 16px;
+  }
+  .collapse_title :before{
+    content:'';
+    display: inline-block;
+    width: 18px;
+    height: 12px;
+    background: url('./../assets/down.png') no-repeat top left;
+    background-size:80%;
+  }
+  .collapse_title a{
+    cursor: pointer;
+  }
+  .collapse_layer{
+    border: 1px solid #ddd;
+    color:#333;
+    font-size:14px;
+    background: #fff;
+    margin-top:-3px;
+    /*display: block;*/
+    height:  0;
+    overflow: hidden;
+    opacity:0;
+  }
+  .collapse_content {
+    padding: 15px;
+  }
+  .collapseIn{
+     position: relative;
+     height: 0;
+     overflow: hidden;
+     -webkit-transition-timing-function: ease;
+     -o-transition-timing-function: ease;
+     transition-timing-function: ease;
+     -webkit-transition-duration: .35s;
+     -o-transition-duration: .35s;
+     transition-duration: .35s;
+     -webkit-transition-property: height;
+     -o-transition-property: height;
+     transition-property: height;
+   }
+  .collapseOut{
+    -webkit-transition-timing-function: ease;
+    -o-transition-timing-function: ease;
+    transition-timing-function: ease;
+    -webkit-transition-duration: .35s;
+    -o-transition-duration: .35s;
+    transition-duration: .35s;
+    -webkit-transition-property: height,opacity;
+    -o-transition-property: height,opacity;
+    transition-property: height,opacity;
+  }
 </style>
