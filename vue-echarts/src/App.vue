15679a8530a0d73a695ad6f1138c002257ae8e86
Dida1209 <duhuiying1209@Outlook.com>
2017-08-14 15:39:08 +0800

    echarts in vue

---
+++ vue-echarts/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+// import EchartsTest from './components/Echarts-test'
+// import Pie from './components/pie'
+import Map from './components/map'
+
+export default {
+  name: 'app',
+  components: {
+    // 'hello':EchartsTest
+    // 'hello': Pie
+    'hello': Map
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
